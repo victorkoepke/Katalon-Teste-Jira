d549eb18c052c0257f858b262371455d82ff2581
victorkoepke <victor.koepke@oklgroup.net>
2020-01-14 18:36:24 -0300

    novo teste, dessa vez com o Github

---
+++ Scripts/Github/Login Test/Script1579037374722.groovy
@@ -0,0 +1,32 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('https://github.com/')
+
+WebUI.click(findTestObject('Object Repository/Github/Page_The worlds leading software developmen_e309d9/a_Signin'))
+
+WebUI.setText(findTestObject('Object Repository/Github/Page_Sign in to GitHub  GitHub/input_Username or email address_login'), 
+    GlobalVariable.username)
+
+WebUI.setText(findTestObject('Object Repository/Github/Page_Sign in to GitHub  GitHub/input_Forgot password_password'), 
+    GlobalVariable.password)
+
+WebUI.click(findTestObject('Object Repository/Github/Page_Sign in to GitHub  GitHub/input_Forgot password_commit'))
+
